6b33e2e9b54b0922c49abd8c797ab41b6b0f4f93
beiyun.zhou <beiyun.zhou@medtap.cn>
2016-12-07 09:51:42 +0800

    Cartfile supported

---
+++ Reflection/Reflection/Reflection/Reflection.h
@@ -0,0 +1,19 @@
+//
+//  Reflection.h
+//  Reflection
+//
+//  Created by jack zhou on 07/12/2016.
+//  Copyright © 2016 jack zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reflection.
+FOUNDATION_EXPORT double ReflectionVersionNumber;
+
+//! Project version string for Reflection.
+FOUNDATION_EXPORT const unsigned char ReflectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reflection/PublicHeader.h>
+
+
